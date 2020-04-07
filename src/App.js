66092d52717c6dcde6d37119089f8818fcbecd12
Roei Berkovich <roei.berkovich@appsflyer.com>
2020-04-07 21:49:36 +0300

    new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import React, { useCallback } from 'react';
 import './App.css';
-import monkey from './assets/monkey.png'
-import activemonkey from './assets/activemonkey.png'
+
 import { useWebSocket } from './hooks/useWS'
 import { useBodyBounderies } from './hooks/useBodyBounderies'
 import  {ws, playerMoves} from './Utills.js/Utills'
 import { useEventListener } from './hooks/useEventListener'
-
+import Monkeys from './components/Monkeys'
 function App() {
   const bodyBounderies = useBodyBounderies()
   const [players, sendMSG] = useWebSocket(ws, bodyBounderies)
@@ -18,16 +17,7 @@ function App() {
   useEventListener('keydown', showKeyCode);
   return (
     <>
-      {players.map(player => {
-        return (
-          <div key={player.id}>
-            <div className={`playerImg ${player.shake === true ? "shake" : ''}`} style={{ bottom: player.p.y + 'px', left: player.p.x + 'px', color: `rgb(${player.color[0]},${player.color[1]},${player.color[2]})` }}>
-              <span style={{ position: "absolute", top: "-40px", left: "-25px" }}>{player.exceptionType}score{player.score}</span>
-              <img alt="player img" src={player.active ? activemonkey : monkey} />
-            </div>
-          </div>
-        )
-      })}
+    <Monkeys players={players} />
     </>
   );
 }
