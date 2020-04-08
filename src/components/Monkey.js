107d1d5df81ba512a952b9c270fe72450a7d1742
Roei Berkovich <roei.berkovich@appsflyer.com>
2020-04-08 09:40:54 +0300

    css

--- src/components/Monkey.js
+++ src/components/Monkey.js
@@ -4,8 +4,8 @@ import activemonkey from '../assets/activemonkey.png'
 const Monkey = ({ shake,p,color,exceptionType,active,score }) => {
     return (
         <>
-            <div className={`playerImg ${shake === true ? "shake" : ''}`} style={{ bottom: p.y + 'px', left: p.x + 'px', color: `rgb(${color[0]},${color[1]},${color[2]})` }}>
-                <span style={{ position: "absolute", top: "-40px", left: "-25px" }}>{exceptionType}score{score}</span>
+            <div className={`playerImg ${shake === true ? "shake" : ''}`} style={{ bottom: p.y + 'px', left: p.x + 'px' }}>
+                <span className="exception">{exceptionType}</span>
                 <img alt="player img" src={active ? activemonkey : monkey} />
             </div>
         </>
